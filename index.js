dc56283fd8b6db17bc0dc7a575347672617e8715
Haardhik <kunderhaardhik@gmail.com>
2023-07-24 18:32:58 +0530

    second commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const app = require("express")()
+// ! making a server and passing express through it
+const server= require("http").createServer(app)
+// ! cors for cross connections
+const cors = require("cors")
+
+// ! socket io video 
+const io= require("socket.io")(server,{
+    cors:{
+        origin: "*",
+        methods: ["GET","POST"]
+    }
+})
+app.use(cors())
+
+const PORT = process.env.PORT || 5000
+
+app.get("/", (req, res) => {
+    res.send("Server is running")
+})
+
+io.on("connection",(socket)=>{
+    socket.emit("me",socket.id)
+    socket.on("disconnect",()=>{
+        socket.broadcast.emit("callended")
+    })
+
+    socket.on("calluser",({userToCall,signalData,from,name})=>{
+      io.to(userToCall).emit("calluser",{signal:signalData,from,name})
+
+    })
+
+    socket.on("answercall",(data)=>{
+        io.to(data.to).emit("callaccepted",data.signal)
+  
+      })
+})
+
+
+
+
+
+server.listen(PORT,()=>console.log(`server listening on port ${PORT}`))
